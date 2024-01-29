24cc787021a9ef87276ee6ae5cbbdf2097ee7518
Kumar <kshitiz.kumar@refinitiv.com>
2024-01-29 19:53:19 +0530

    commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SignupPage from './SignupPage';
+function App() {
+  return (
+    <div className="App">
+      <SignupPage/>
+    </div>
+  );
+}
+
+export default App;
